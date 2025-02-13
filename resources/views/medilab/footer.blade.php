6843d3bd9066ac2ddb684dc582726c8ebe56726c
nasrulnfc <nasrul99@gmail.com>
2025-02-13 16:21:41 +0700

    Penambahan Keterangan Footer

--- resources/views/medilab/footer.blade.php
+++ resources/views/medilab/footer.blade.php
@@ -1,6 +1,10 @@
 <footer>
     <div class="container copyright text-center mt-4">
-        <p>© <span>Copyright</span> <strong class="px-1 sitename">SISEHAT</strong> <span>All Rights Reserved</span></p>
+        <span>Copyright</span> <strong class="px-1 sitename">&copy; SISEHAT</strong>
+        <span>All Rights Reserved</span><br/>
+        <span>Sistem Informasi Ini Didukung Oleh: </span>
+        <strong class="px-1 sitename"><a href="https://nurulfikri.ac.id/" target="_blank"> STT Terpadu Nurul
+                Fikri</a></strong></p>
 
     </div>
 
